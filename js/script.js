d20585bccf6ad2f7dbf8a63b58ee5c1bd77a4837
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-25 14:57:48 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $ Snack 1
+// $ Creare un array di oggetti:
+// $ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// $ Stampare a schermo la bici con peso minore.
+
+// # functions
+
+function Bicycle(name, weight) {
+  this.name = name;
+  this.weightValue = weight;
+  this.weightUnit = "kg";
+}
