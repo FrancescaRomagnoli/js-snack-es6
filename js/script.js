aa15d961b5aa42cbbf20c1bafbff9610cf275571
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-25 15:42:06 +0200

    add second snack

--- js/script.js
+++ js/script.js
@@ -33,3 +33,26 @@ for (i = 1; i < bicycleList.length; i++) {
 }
 
 console.log(lightestBicycle.name);
+
+// $ Snack 2
+// $ Creare un array di oggetti di squadre di calcio.
+// $  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// $ Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+// $ Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
+// $ Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// # functions
+
+function footballTeam(name) {
+  this.name = name;
+  this.points = 0;
+  this.fouls = 0;
+}
+
+// # objects
+
+const footballTeam1 = new footballTeam("AFC Richmond");
+const footballTeam2 = new footballTeam("Crystal Palace F.C.");
+const footballTeam3 = new footballTeam("West Ham United F.C.");
+
+const FootballTeams = [footballTeam1, footballTeam2, footballTeam3];
