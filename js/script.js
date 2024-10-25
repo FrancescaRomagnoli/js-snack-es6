fc570788f52c7c3d8fde69a10a5c7cadde8351e5
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-25 15:20:13 +0200

    add first snack

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ function Bicycle(name, weight) {
   this.weightValue = weight;
   this.weightUnit = "kg";
 }
+
+// # objects
+
+const bicycleOne = new Bicycle("Bicycle One", 30);
+const bicycleTwo = new Bicycle("Bicycle Two", 25);
+const bicycleThree = new Bicycle("Bicycle Three", 40);
+
+// # objects array
+
+const bicycleList = [bicycleOne, bicycleTwo, bicycleThree];
+console.log(bicycleList);
+
+let lightestBicycle = bicycleList[0];
+
+for (i = 1; i < bicycleList.length; i++) {
+  let currentBicycle = bicycleList[i];
+
+  if (currentBicycle.weightValue < lightestBicycle.weightValue) {
+    lightestBicycle = currentBicycle;
+  }
+}
+
+console.log(lightestBicycle.name);
